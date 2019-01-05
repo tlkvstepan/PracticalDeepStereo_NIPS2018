d68ba651ea8c224eaf88661fa9e5d70533100c99
tulyakov <tulyakov.stepan@gmail.com>
2019-01-05 19:58:24 +0100

    Added 3d convolutions and deconvolutions to modules.py.

--- practical_deep_stereo/modules.py
+++ practical_deep_stereo/modules.py
@@ -6,6 +6,16 @@
 from torch import nn
 
 
+def convolution_3x3x3(number_of_input_features, number_of_output_features,
+                      stride):
+    return nn.Conv3d(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=3,
+        stride=stride,
+        padding=1)
+
+
 def convolution_3x3(number_of_input_features, number_of_output_features):
     return nn.Conv2d(
         number_of_input_features,
@@ -24,21 +34,101 @@ def convolution_5x5_stride_2(number_of_input_features,
         padding=2)
 
 
-def convolutional_block_5x5_stride_2(number_of_input_features,
-                                     number_of_output_features):
+def transposed_convolution_3x4x4_stride_122(number_of_input_features,
+                                            number_of_output_features):
+    return nn.ConvTranspose3d(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=(3, 4, 4),
+        stride=(1, 2, 2),
+        padding=(1, 1, 1))
+
+
+def convolutional_block_2D(number_of_input_features, number_of_output_features,
+                           kernel_size, stride):
     return nn.Sequential(
-        convolution_5x5_stride_2(number_of_input_features,
-                                 number_of_output_features),
+        nn.Conv2d(
+            number_of_input_features,
+            number_of_output_features,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=kernel_size // 2),
         nn.LeakyReLU(negative_slope=0.1, inplace=True),
         nn.InstanceNorm2d(number_of_output_features, affine=True))
+
+
+def convolution_block_3D(number_of_input_features, number_of_output_features,
+                         kernel_size, stride):
+    return nn.Sequential(
+        nn.Conv3d(
+            number_of_input_features,
+            number_of_output_features,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=kernel_size // 2),
+        nn.LeakyReLU(negative_slope=0.1, inplace=True),
+        nn.InstanceNorm3d(number_of_output_features, affine=True))
+
+
+def transposed_convololution_3D(number_of_input_features,
+                                number_of_output_features,
+                                kernel_size, stride,
+                                padding):
+    return nn.Sequential(
+        nn.ConvTranspose3d(
+            number_of_input_features,
+            number_of_output_features,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding),
+        nn.LeakyReLU(negative_slope=0.1, inplace=True),
+        nn.InstanceNorm3d(number_of_output_features, affine=True))
+
+
+def convolutional_block_5x5_stride_2(number_of_input_features,
+                                     number_of_output_features):
+    return convolutional_block_2D(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=5,
+        stride=2)
 
 
 def convolutional_block_3x3(number_of_input_features,
                             number_of_output_features):
-    return nn.Sequential(
-        convolution_3x3(number_of_input_features, number_of_output_features),
-        nn.LeakyReLU(negative_slope=0.1, inplace=True),
-        nn.InstanceNorm2d(number_of_output_features, affine=True))
+    return convolutional_block_2D(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=3,
+        stride=1)
+
+
+def convolutional_block_3x3x3(number_of_input_features,
+                              number_of_output_features):
+    return convolution_block_3D(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=3,
+        stride=1)
+
+
+def convolutional_block_3x3x3_stride_2(number_of_input_features,
+                                       number_of_output_features):
+    return convolution_block_3D(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=3,
+        stride=2)
+
+
+def transposed_convolutional_block_4x4x4_stride_2(number_of_input_features,
+                                                  number_of_output_features):
+    return transposed_convololution_3D(
+        number_of_input_features,
+        number_of_output_features,
+        kernel_size=4,
+        stride=2,
+        padding=1)
 
 
 class ResidualBlock(nn.Module):
