39a45c40cd538500b3e1ff5d75887c7db0361510
tulyakov <tulyakov.stepan@gmail.com>
2019-02-22 13:34:25 +0100

    Fixed bug in error estimation.
    Detials:
    In situation where all ground truth is not avaliable function
    returns 0 error (similar to psm).

--- practical_deep_stereo/errors.py
+++ practical_deep_stereo/errors.py
@@ -13,24 +13,30 @@ def compute_absolute_error(estimated_disparity,
 
     Locations where ground truth is not avaliable do not contribute to mean
     absolute error. In such locations pixel-wise error is shown as zero.
+    If ground truth is not avaliable in all locations, function returns 0.
 
     Args:
         ground_truth_disparity: ground truth disparity where locations with
                                 unknow disparity are set to inf's.
         estimated_disparity: estimated disparity.
-        use_mean_to_average: if True than use mean to pixelwise errors,
-                             otherwise use median.
+        use_mean: if True than use mean to average pixelwise errors,
+                  otherwise use median.
     """
     absolute_difference = (estimated_disparity - ground_truth_disparity).abs()
     locations_without_ground_truth = th.isinf(ground_truth_disparity)
     pixelwise_absolute_error = absolute_difference.clone()
     pixelwise_absolute_error[locations_without_ground_truth] = 0
-    if use_mean:
-        average_absolute_error = \
-            absolute_difference[~locations_without_ground_truth].mean().item()
+    absolute_differece_with_ground_truth = absolute_difference[
+        ~locations_without_ground_truth]
+    if absolute_differece_with_ground_truth.numel() == 0:
+        average_absolute_error = 0.0
     else:
-        average_absolute_error = \
-            absolute_difference[~locations_without_ground_truth].median().item()
+        if use_mean:
+            average_absolute_error = absolute_differece_with_ground_truth.mean(
+            ).item()
+        else:
+            average_absolute_error = absolute_differece_with_ground_truth.median(
+            ).item()
     return pixelwise_absolute_error, average_absolute_error
 
 
@@ -41,7 +47,9 @@ def compute_n_pixels_error(estimated_disparity, ground_truth_disparity, n=3.0):
     n-pixel error. In such locations pixel-wise error is shown as zero.
 
     Note that n-pixel error is equal to one if
-    |estimated_disparity-ground_truth_disparity| > 1 and zero otherwise.
+    |estimated_disparity-ground_truth_disparity| > n and zero otherwise.
+
+    If ground truth is not avaliable in all locations, function returns 0.
 
     Args:
         ground_truth_disparity: ground truth disparity where locations with
@@ -55,6 +63,12 @@ def compute_n_pixels_error(estimated_disparity, ground_truth_disparity, n=3.0):
         estimated_disparity - ground_truth_disparity).abs().gt(n).float()
     pixelwise_n_pixels_error = more_than_n_pixels_absolute_difference.clone()
     pixelwise_n_pixels_error[locations_without_ground_truth] = 0.0
-    percentage_of_pixels_with_error = more_than_n_pixels_absolute_difference[
-        ~locations_without_ground_truth].mean().item() * 100
+    more_than_n_pixels_absolute_difference_with_ground_truth = \
+        more_than_n_pixels_absolute_difference[~locations_without_ground_truth]
+    if more_than_n_pixels_absolute_difference_with_ground_truth.numel() == 0:
+        percentage_of_pixels_with_error = 0.0
+    else:
+        percentage_of_pixels_with_error = \
+            more_than_n_pixels_absolute_difference_with_ground_truth.mean(
+                ).item() * 100
     return pixelwise_n_pixels_error, percentage_of_pixels_with_error
--- test/test_errors.py
+++ test/test_errors.py
@@ -10,13 +10,19 @@ import torch as th
 from practical_deep_stereo import errors
 
 
-def _check_compute_absolute_error(estimated_disparity, ground_truth_disparity):
+def test_compute_absolute_error():
+    # yapf: disable
+    estimated_disparity = th.Tensor([[1.0, 2.0],
+                                     [3.0, 4.0]])
+    ground_truth_disparity = th.Tensor([[2.0, 2.0],
+                                        [float('inf'), 1.0]])
+    # yapf: enable
     (pixelwise_absolute_error,
      mean_absolute_error) = errors.compute_absolute_error(
          estimated_disparity, ground_truth_disparity, use_mean=True)
-    # yapf: disable 
+    # yapf: disable
     assert th.all(
-        th.isclose(pixelwise_absolute_error, th.Tensor([[1.0, 0.0], 
+        th.isclose(pixelwise_absolute_error, th.Tensor([[1.0, 0.0],
                                                         [0.0, 3.0]])))
     # yapf: enable
     assert math.isclose(mean_absolute_error, 4.0 / 3.0, rel_tol=1e-3)
@@ -24,14 +30,26 @@ def _check_compute_absolute_error(estimated_disparity, ground_truth_disparity):
      median_absolute_error) = errors.compute_absolute_error(
          estimated_disparity, ground_truth_disparity, use_mean=False)
     assert math.isclose(median_absolute_error, 1.0, rel_tol=1e-3)
-    # yapf: disable 
+    # yapf: disable
     assert th.all(
-        th.isclose(pixelwise_absolute_error, th.Tensor([[1.0, 0.0], 
+        th.isclose(pixelwise_absolute_error, th.Tensor([[1.0, 0.0],
                                                         [0.0, 3.0]])))
     # yapf: enable
+    # Test case when no ground truth is avaliable.
+    ground_truth_disparity = th.full((2, 2), float('inf'))
+    (pixelwise_absolute_error,
+     mean_absolute_error) = errors.compute_absolute_error(
+         estimated_disparity, ground_truth_disparity, use_mean=True)
+    assert mean_absolute_error == 0.0
 
 
-def _check_compute_n_pixels_error(estimated_disparity, ground_truth_disparity):
+def test_compute_n_pixels_error():
+    # yapf: disable
+    estimated_disparity = th.Tensor([[1.0, 2.0],
+                                     [3.0, 4.0]])
+    ground_truth_disparity = th.Tensor([[2.0, 2.0],
+                                        [float('inf'), 1.0]])
+    # yapf: enable
     (pixelwise_n_pixels_error, n_pixels_error) = errors.compute_n_pixels_error(
         estimated_disparity, ground_truth_disparity, n=1.0)
     # yapf: disable
@@ -40,15 +58,8 @@ def _check_compute_n_pixels_error(estimated_disparity, ground_truth_disparity):
                                                         [0.0, 1.0]])))
     # yapf: enable
     assert math.isclose(n_pixels_error, 100.0 / 3.0, rel_tol=1e-3)
-
-
-def test_errors():
-    # yapf: disable
-    estimated_disparity = th.Tensor([[1.0, 2.0],
-                                     [3.0, 4.0]])
-    ground_truth_disparity = th.Tensor([[2.0, 2.0],
-                                        [float('inf'), 1.0]])
-    # yapf: enable
-    _check_compute_absolute_error(estimated_disparity,
-                                         ground_truth_disparity)
-    _check_compute_n_pixels_error(estimated_disparity, ground_truth_disparity)
+    # Test case when no ground truth is avaliable.
+    ground_truth_disparity = th.full((2, 2), float('inf'))
+    (pixelwise_n_pixels_error, n_pixels_error) = errors.compute_n_pixels_error(
+        estimated_disparity, ground_truth_disparity, n=1.0)
+    assert n_pixels_error == 0.0
